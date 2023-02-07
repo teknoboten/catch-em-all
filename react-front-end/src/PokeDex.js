9f290927df036a51ce54bed800506a8a29e984b0
serra <serra@boten.ca>
2023-02-07 11:26:07 -0800

    add thumbnail images

--- react-front-end/src/PokeDex.js
+++ react-front-end/src/PokeDex.js
@@ -4,10 +4,10 @@ import PokeCard from './PokeCard'
 
 export default function PokeDex({ pokemon }) {
   return (
-    <Grid container spacing={3} sx={{ marginTop: '5vh' }}>
+    <Grid container spacing={4} sx={{ marginTop: '5vh' }}>
       {pokemon.map((p, index) => {
         return (
-          <Grid item xs={4} key={index}>
+          <Grid item sm={12} md={6} lg={4} key={index}>
             <PokeCard pokemon={p} />
           </Grid>
         )
